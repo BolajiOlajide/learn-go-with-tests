0c7f04eebc8f06aca8515873baf5166606df76b1
BolajiOlajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-01-24 03:16:24 +0100

    feat: learn about DI with tests

---
+++ dependencyinjection/api.go
@@ -0,0 +1,14 @@
+package dependencyinjection
+
+import (
+	"log"
+	"net/http"
+)
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler)))
+}
